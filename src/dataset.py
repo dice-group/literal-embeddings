07f870c7507c52811c0a98205f46ca39b2ba7d15
sapkotaruz11 <sapkotarupesh15@gmail.com>
2025-02-18 11:02:04 +0100

    update dataset structure

--- src/dataset.py
+++ src/dataset.py
@@ -1,99 +1,112 @@
-import os
-
-import pandas as pd
 import torch
+import pandas as pd
+import os
+from dicee.knowledge_graph_embeddings import KGE
 
 
 class LiteralData:
-    def __init__(self, dataset_dir: str, ent_idx, filter_df=False, top_k = 10):
+    def __init__(
+        self, dataset_dir: str, ent_idx, filter_df=False, normalization="z-norm"
+    ):
+        self.dataset_dir = dataset_dir
+        self.file_paths = {
+            "train": os.path.join(self.dataset_dir, "train.txt"),
+            "test": os.path.join(self.dataset_dir, "test.txt"),
+            "val": os.path.join(self.dataset_dir, "val.txt"),
+        }
 
-        self.train_file_path = os.path.join(dataset_dir, "train.txt")
-        self.test_file_path = os.path.join(dataset_dir, "test.txt")
-        self.val_file_path = os.path.join(dataset_dir, "valid.txt")
-        self.ent_idx = {value: idx for idx, value in ent_idx["entity"].items()}
+        self.normalization_params = {}
+        self.normalization = normalization
+        if isinstance(ent_idx, dict):
+            self.entity_to_idx = ent_idx
+        else:
+            self.entity_to_idx = {
+                value: idx for idx, value in ent_idx["entity"].items()
+            }
+        self.preprocess_train()
 
+    def preprocess_train(self):
+        train_file_path = self.file_paths.get("train", None)
+        assert os.path.exists(
+            train_file_path
+        ), f"The path {train_file_path} does not exist."
         df = pd.read_csv(
-            self.train_file_path,
+            train_file_path,
             sep="\t",
             header=None,
             names=["head", "relation", "tail"],
         )
-
-        if filter_df:
-            top_k_rels = df["relation"].value_counts().nlargest(top_k).index
-
-            # Step 2: Filter the DataFrame to include only rows with these top 10 items
-            df = df[df["relation"].isin(top_k_rels)]
-            self.train_rels = top_k_rels
-        else:
-            self.train_rels = df["relation"].unique().tolist()
-
-        df = df[df["head"].isin(self.ent_idx)]
-        df["tail"] = df["tail"].astype(float)
-
-        self.unique_relations = df["relation"].unique()
-        self.num_data_properties = len(self.unique_relations)
-        
         self.data_property_to_idx = {
-            relation: idx for idx, relation in enumerate(self.unique_relations)
+            relation: idx for idx, relation in enumerate(df["relation"].unique())
         }
-
-        df["head_idx"] = df["head"].map(self.ent_idx)
+        self.num_data_properties = len(self.data_property_to_idx)
+        df = df[df["head"].isin(self.entity_to_idx)]
+        df["head_idx"] = df["head"].map(self.entity_to_idx)
         df["rel_idx"] = df["relation"].map(self.data_property_to_idx)
+        self.normalize(df)
 
-        # Calculate normalization parameters for each relation group
-        self.normalization_params = {}
-        for relation in self.unique_relations:
-            group_data = df.loc[df["relation"] == relation, "tail"]
-            mean = group_data.mean()
-            std = group_data.std()
-            self.normalization_params[relation] = {"mean": mean, "std": std}
+        # Compute mean and std for each rel_idx in a vectorized manner
 
-        # Normalize the tail values using the stored parameters
-        df["normalized_tail"] = (
-            df["tail"] - df.groupby("relation")["tail"].transform("mean")
-        ) / df.groupby("relation")["tail"].transform("std")
-        self.train_df = df
-        # Pivot to create a new DataFrame where rows are indexed by 'head' and columns are literals
-        normalized_scores = df.pivot_table(
-            index="head", columns="relation", values="normalized_tail", aggfunc="first"
-        ).reset_index()
+        self.train_data = {
+            "triples": torch.tensor(
+                df[["head_idx", "rel_idx"]].values, dtype=torch.long
+            ),
+            "tails": torch.tensor(df[["tail"]].values, dtype=torch.float),
+            "tails_norm": torch.tensor(df[["tail_norm"]].values, dtype=torch.float32),
+        }
+        print()
 
-        # Handle missing heads efficiently
-        missing_heads = set(self.ent_idx.keys()) - set(df["head"])
-        if missing_heads:
-            missing_rows = pd.DataFrame({"head": list(missing_heads)})
+    def get_df(self, split="test"):
+        file_path = self.file_paths.get(split, None)
 
-            # Concatenate missing rows with normalized_scores
-            normalized_scores = pd.concat(
-                [normalized_scores, missing_rows], ignore_index=True
+        if file_path is None or not os.path.isfile(file_path):
+            raise ValueError(
+                f"Invalid data split or path does not lead to a file: {file_path}"
             )
 
-        # Fill missing values with 0
-        normalized_scores.fillna(0, inplace=True)
+        df = pd.read_csv(
+            file_path,
+            sep="\t",
+            header=None,
+            names=["head", "relation", "tail"],
+        )
+        df["head_idx"] = df["head"].map(self.entity_to_idx)
+        df["rel_idx"] = df["relation"].map(self.data_property_to_idx)
+        return df
 
-        # Add 'head_index' column
-        normalized_scores["head_index"] = normalized_scores["head"].map(self.ent_idx)
+    def normalize(self, df):
+        if self.normalization == "z-norm":
+            normalization_params_df = df.groupby("rel_idx")["tail"].agg(["mean", "std"])
 
-        # Get column names dynamically from self.data_property_to_idx
-        columns = ["head_index"] + list(self.data_property_to_idx.keys())
+            # Convert to dictionary if needed (optional)
+            self.normalization_params = normalization_params_df.to_dict(orient="index")
+            df["tail_norm"] = df.groupby("rel_idx")["tail"].transform(
+                lambda x: (x - x.mean()) / x.std()
+            )
+        elif self.normalization == "min-max":
+            normalization_params_df = df.groupby("rel_idx")["tail"].agg(["min", "max"])
 
-        # Reorder and sort by 'head_index'
-        self.normalized_df = (
-            normalized_scores[columns]
-            .sort_values(by="head_index")
-            .reset_index(drop=True)
+            # Convert to dictionary if needed (optional)
+            self.normalization_params = normalization_params_df.to_dict(orient="index")
+            df["tail_norm"] = df.groupby("rel_idx")["tail"].transform(
+                lambda x: (x - x.min()) / (x.max() - x.min())
+            )
+
+    def get_onehot_batch(self, entity_idx: torch.tensor):
+        entity_idx = entity_idx.to("cpu")
+        indices = torch.where(torch.isin(self.train_data["triples"][:, 0], entity_idx))[
+            0
+        ]
+        entids = self.train_data["triples"][indices, 0]
+        rels = self.train_data["triples"][indices, 1]
+        lables = self.train_data["tails_norm"][indices]
+
+        # Create a zero tensor of shape [num_samples, num_relations]
+        num_samples = entids.shape[0]  # Number of rows in dataset
+        y_true = torch.zeros(
+            (num_samples, self.num_data_properties), dtype=torch.float32
         )
 
-        # Convert selected columns to PyTorch tensor
-        self.lit_value_tensor = torch.tensor(
-            self.normalized_df[self.data_property_to_idx.keys()].values,
-            dtype=torch.float32,
-        )
-
-        self.lit_bool_tensor = torch.tensor(
-            (self.normalized_df[self.data_property_to_idx.keys()].values != 0).astype(
-                int
-            ),
-            dtype=torch.int32,
-        )
+        # Assign tail values at the correct relation indices per row
+        y_true[torch.arange(num_samples), rels] = lables.squeeze()
+        return entids, rels, y_true
